a0ce33935674fd64d4e2a497c6149aba6528cb15
Rushmore Mushambi <rushmore@surrealdb.com>
2024-06-06 17:46:12 +0200

    Sent queries as text

--- lib/src/api/engine/remote/ws/native.rs
+++ lib/src/api/engine/remote/ws/native.rs
@@ -237,7 +237,7 @@ pub(crate) fn router(
 							let (id, method, param) = request;
 							let params = match param.query {
 								Some((query, bindings)) => {
-									vec![query.into(), bindings.into()]
+									vec![query.to_string().into(), bindings.into()]
 								}
 								None => param.other,
 							};
